696103bace22d409f13ba3c25e2c8a19e4497da2
claudia <claudia_figueroai@hotmail.com>
2021-08-16 16:33:57 -0400

    se agregan funcionalidades a los botones para el efecto acordeon a las tarjetas

---
+++ assets/js/tarjetita.js
@@ -0,0 +1,19 @@
+// Guardar el estado (columna activa)
+let columnaActiva = 1
+
+// Selecionar las columnas
+const columnas = document.querySelectorAll(".columna")
+
+// Escuchar los "clicks" en cada una de ellas
+columnas.forEach((columna, indice) => {
+    columna.addEventListener("click", function() {
+        cambiarColumna(indice)
+    })    
+})
+
+// Cambiar el estado de las columnas
+function cambiarColumna(indice) {
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva = indice
+}
